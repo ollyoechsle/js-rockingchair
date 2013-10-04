76e2895d57a744bfd7445ccd9ec6a658c78508ff
Olly Oechsle <owksley@gmail.com>
2013-10-04 16:58:23 +0100

    Adds a bar underneath

--- javascript/viewmodel.js
+++ javascript/viewmodel.js
@@ -5,35 +5,47 @@
         self.x = ko.observable(x);
         self.y = ko.observable(y);
 
-        self.render = function(ctx) {
-            ctx.fillRect(self.x(), self.y(), 5, 5);
+        self.render = function (ctx) {
+            ctx.fillRect(self.x() - 3, self.y() - 3, 6, 6);
         }
     }
 
     function BracketViewModel(hangingPoint, length) {
         var self = this;
 
-        self.angle = 0;
+        self.angle = Math.PI / 2;
 
-        var endPoint = {
-            x: function() {
+        self.endPoint = {
+            x: function () {
                 return hangingPoint.x() + (Math.cos(self.angle) * length);
             },
-            y: function() {
+            y: function () {
                 return hangingPoint.y() + (Math.sin(self.angle) * length);
             }
         };
 
-        self.render = function(ctx) {
+        self.render = function (ctx) {
 
             ctx.beginPath();
             ctx.moveTo(hangingPoint.x(), hangingPoint.y());
-            ctx.lineTo(endPoint.x(), endPoint.y());
+            ctx.lineTo(self.endPoint.x(), self.endPoint.y());
             ctx.stroke();
 
-            self.angle++;
+        }
+    }
+
+    function BarViewModel(bracket1, bracket2) {
+        var self = this;
+
+        self.render = function (ctx) {
+
+            ctx.beginPath();
+            ctx.moveTo(bracket1.endPoint.x(), bracket1.endPoint.y());
+            ctx.lineTo(bracket2.endPoint.x(), bracket2.endPoint.y());
+            ctx.stroke();
 
         }
+
     }
 
     function RockingChairViewModel() {
@@ -42,10 +54,12 @@
         self.hangingPoint1 = new PointViewModel(10, 10);
         self.hangingPoint2 = new PointViewModel(50, 10);
 
-        self.bar1 = new BracketViewModel(self.hangingPoint1, 20);
-        self.bar2 = new BracketViewModel(self.hangingPoint2, 20);
+        self.bracket1 = new BracketViewModel(self.hangingPoint1, 20);
+        self.bracket2 = new BracketViewModel(self.hangingPoint2, 20);
 
-        self.objects = [self.hangingPoint1, self.hangingPoint2, self.bar1, self.bar2];
+        self.bar1 = new BarViewModel(self.bracket1, self.bracket2);
+
+        self.objects = [self.hangingPoint1, self.hangingPoint2, self.bracket1, self.bracket2, self.bar1];
 
     }
 
